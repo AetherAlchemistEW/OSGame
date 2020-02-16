6bb63515e7628d80f629d7e793c92b04112e167d
Ed <ewhitehead@aetheralchemist.com.au>
2020-02-16 22:36:39 +1100

    Map integration
    Leaflet/Open Maps

--- ggj2020/lib/pages/map.dart
+++ ggj2020/lib/pages/map.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+import 'package:location/location.dart';
 
-class Map extends StatelessWidget {
+class Map extends StatefulWidget {
+
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: Container(
-          child: Column(
-          children: <Widget>[
-            Text("New Page"),
-            FloatingActionButton(
-              onPressed: () => Navigator.pop(context),
-            )
-          ],
-        ),
-      )
+      child: Stack(
+        children: <Widget>[
+          new FlutterMap(
+            options: new MapOptions(
+              center: new LatLng(-37, 144),
+              zoom: 13.0,
+            ),
+            layers: [
+              new TileLayerOptions(
+                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a','b','c'],
+                maxZoom: 5,
+              ),
+              new MarkerLayerOptions(
+                markers: [
+                  new Marker(
+                    width: 80.0,
+                    height: 80.0,
+                    point: new LatLng(-37, 144),
+                    builder: (ctx) =>
+                    new Container(
+                      child: new FlutterLogo(),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          FloatingActionButton(
+            onPressed: () => Navigator.pop(context),
+            elevation: 2,
+            backgroundColor: Colors.black87,
+            tooltip: 'Exit Map',
+            child: Icon(Icons.transit_enterexit, color: Theme.of(context).iconTheme.color,),
+          ),
+        ],
+      ),
     );
   }
 }
